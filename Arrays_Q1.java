d78fcc54fa0ca5e61ea04a00e13ea794eb48ecf7
kaustavv-b <kaustav04b@gmail.com>
2026-03-28 18:59:11 +0530

    Update Arrays_Q1.java

--- Arrays_Q1.java
+++ Arrays_Q1.java
@@ -1,25 +1,23 @@
 class Arrays_Q1 {
     public int subarraySum(int[] nums, int k) {
         int start = 0, end = 0, ans = 0, n=nums.length;
-        while (start < nums.length) {
+        while (start<n) {
             int sum = 0;
-            // compute sum from start to end
-            for (int i = start; i <= end && i < n; i++) {
+            for(int i=start; i<=end && i<n; i++)
                 sum += nums[i];
-            }
-
-            if (sum == k) {
+            if (sum==k){
                 ans++;
                 end++;
-            } else if (sum < k)
+            } 
+            else if (sum<k)
                 end++;
-            else {
+            else{
                 start++;
                 if (start > end) {
                     end = start;
                 }
             }
-            if (end >= nums.length) {
+            if (end >= n) {
                 start++;
                 end = start;
             }
